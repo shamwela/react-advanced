aa15d1eefbae4a8427b72a7a23d1ce3eb7b53108
Sha Mwe La <shamwela2002@gmail.com>
2021-07-23 18:04:02 +0630

    9 - Custom Hooks

--- src/hooks/Counter.jsx
+++ src/hooks/Counter.jsx
@@ -1,16 +1,11 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
+import useDocumentTitle from './useDocumentTitle'
 
 export default function Counter() {
   const [count, setCount] = useState(0)
   const [name, setName] = useState('')
 
-  useEffect(() => {
-    document.title = `${name} has clicked ${count} times.`
-
-    return () => {
-      console.log('Clean up')
-    }
-  })
+  useDocumentTitle(`${name} has clicked ${count} times.`)
 
   return (
     <>
